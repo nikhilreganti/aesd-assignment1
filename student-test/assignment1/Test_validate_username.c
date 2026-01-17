f760108df2cc02cfd5d21409afff81a89570f145
Nikhil Reganti <nikhilreganti@gmail.com>
2026-01-17 16:05:10 +0530

    Implement username validation test

--- student-test/assignment1/Test_validate_username.c
+++ student-test/assignment1/Test_validate_username.c
@@ -14,9 +14,17 @@
 */
 void test_validate_my_username()
 {
-    /**
-     * TODO: Replace the line below with your code here as described above to verify your /conf/username.txt 
-     * config file and my_username() functions are setup properly
-     */
-    TEST_ASSERT_TRUE_MESSAGE(false,"AESD students, please fix me!");
+    const char *hardcoded_username = my_username();
+    char *conf_username = malloc_username_from_conf_file();
+
+    TEST_ASSERT_NOT_NULL_MESSAGE(conf_username, "Username from conf file is NULL");
+
+    TEST_ASSERT_EQUAL_STRING_MESSAGE(
+        hardcoded_username,
+        conf_username,
+        "Username in autotest-validate.c does not match conf/username.txt"
+    );
+
+    free(conf_username);
 }
+
